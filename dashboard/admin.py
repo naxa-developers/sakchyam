8cf6386d7c89e77203399c2012c7857e061f09fa
SandipNup <sandipneupane65@gmail.com>
2020-03-06 11:20:40 +0545

    user role

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from dashboard.models import UserRole
+from django.contrib.auth.models import Permission
 
 # Register your models here.
+
+admin.site.register(UserRole)
+admin.site.register(Permission)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User, Group, Permission
 
 # Create your models here.
+
+
+class UserRole(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_role')
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='user_role')
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE, related_name='user_role')
+
+    def __str__(self):
+        return self.user.username
+
