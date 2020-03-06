bb4bcca6530f44ed7e790427db787a861cfbc9c8
SandipNup <sandipneupane65@gmail.com>
2020-03-06 14:40:16 +0545

    forms

---
+++ dashboard/forms.py
@@ -0,0 +1,32 @@
+from django.forms import ModelForm
+from api.models import LogData, LogCategory, LogSubCategory, MilestoneYear, Title
+
+
+class LogCategoryForm(ModelForm):
+    class Meta:
+        model = LogCategory
+        fields = '__all__'
+
+
+class LogDataForm(ModelForm):
+    class Meta:
+        model = LogData
+        fields = '__all__'
+
+
+class LogSubCategoryForm(ModelForm):
+    class Meta:
+        model = LogSubCategory
+        fields = '__all__'
+
+
+class MilestoneYearForm(ModelForm):
+    class Meta:
+        model = MilestoneYear
+        fields = '__all__'
+
+
+class TitleForm(ModelForm):
+    class Meta:
+        model = Title
+        fields = '__all__'
